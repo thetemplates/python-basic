6c325f8efc151f81609bf3cb4c59e1fc2b80a2be
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-21 19:13:30 +0000

    Add src/algorithms/random.py

---
+++ src/algorithms/random.py
@@ -0,0 +1,37 @@
+"""
+Module random.py
+"""
+import numpy as np
+
+import config
+
+
+class Random:
+    """
+    Description
+    -----------
+    This class returns a random number within [0.0   999.99)    
+    """
+
+    def __init__(self) -> None:
+        """
+        The constructor
+        """
+        
+        # Constructing a random generator
+        seed = config.Config().SEED
+        self.__rng = np.random.default_rng(seed=seed)
+
+        # The random number boundaries
+        self.__minimum: float = 0.0
+        self.__maximum: float = 999.99
+
+    def exc(self) -> float:
+        """
+        
+        :return: A random number
+        """
+
+        value: float = (self.__maximum - self.__minimum) * self.__rng.random() + self.__minimum
+
+        return value
