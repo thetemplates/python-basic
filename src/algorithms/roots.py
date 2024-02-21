bbb30c02092df911b37ad9c327ddee79e08046f7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-21 19:13:52 +0000

    Add src/algorithms/roots.py

---
+++ src/algorithms/roots.py
@@ -0,0 +1,24 @@
+"""
+Module root.py
+"""
+
+import math
+
+
+class Roots:
+    """
+    Description
+    -----------
+    A class that returns the square root of integer values only.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def exc(self, value: float) -> float:
+        """
+        
+        :param value: An integer value
+        """
+
+        return math.sqrt(value)
